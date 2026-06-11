d3beebc38da178241c561f69215bddc82441adba
Konataware <luansiloe404@gmail.com>
2026-06-11 12:10:15 -0300

    Implementation of busybox, proper stdio and mount of fs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal, sigprocmask, SigmaskHow};
+use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
-use nix::unistd::{fork, ForkResult, execvp, pause, setpgid, getpgrp, Pid};
-use nix::errno::Errno;
+use nix::unistd::{fork, ForkResult, execvp, pause, getpgrp};
+use nix::mount::{ MsFlags };
 use std::ffi::CString;
+use std::fs::create_dir_all;
 use std::process::exit;
 use std::sync::atomic::{AtomicBool, Ordering};
 use libc;
@@ -23,6 +24,46 @@ extern "C" fn handle_sigchld(_: i32) {
     }
 }
 
+fn mount_filesystems() {
+    // creates directories in case they don't exist
+    let _ = create_dir_all("/dev");
+    let _ = create_dir_all("/proc");
+    let _ = create_dir_all("/sys");
+
+    // mount devtmpfs (/dev/null, /dev/console/, /dev/tty [...])
+    if let Err(e) = nix::mount::mount(
+        Some("devtmpfs"),
+        "/dev",
+        Some("devtmpfs"),
+        MsFlags::empty(),
+        None::<&str>,
+    ) {
+        eprintln!("[ERROR] Failed to mount devtmpfs: {}", e);
+    } else {
+        println!("Mounted devtmpfs on /dev");
+    }
+
+    // mount proc (ps, top, [...])
+    if let Err(e) = nix::mount::mount(
+        Some("/proc"),
+        "/proc",
+        Some("proc"),
+        MsFlags::empty(),
+        None::<&str>,
+    ) {
+        eprintln!("Failed to mount proc: {}", e);
+    }
+
+    // mounts sysfs
+    let _ = nix::mount::mount(
+        Some("sysfs"),
+        "/sys",
+        Some("sysfs"),
+        MsFlags::empty(),
+        None::<&str>
+    );
+}
+
 fn setup_signal_handler() -> Result<(), nix::Error> {
     let handler = SigHandler::Handler(handle_sigchld);
     let sig_action = SigAction::new(
@@ -40,6 +81,8 @@ fn main() {
         exit(1);
     }
 
+    mount_filesystems();
+
     // ignoring SIGTTOU so the parent doesn't get stopped when child calls tcsetpgrp
     let ignore_action = SigAction::new(
         SigHandler::SigIgn,
@@ -48,7 +91,6 @@ fn main() {
     );
     unsafe {
         let _ = sigaction(Signal::SIGTTOU, &ignore_action);
-        let _ = sigaction(Signal::SIGTTIN, &ignore_action);
     }
 
     match unsafe { fork() } {
@@ -64,40 +106,23 @@ fn main() {
         #[allow(unused_unsafe)]
         #[allow(unreachable_code)]
         Ok(ForkResult::Child) => {
-
+            // turns into session leader
             unsafe { libc::setsid(); }
-            // places child into its own process group
-            setpgid(Pid::from_raw(0), Pid::from_raw(0)).expect("setpgid failed");
 
+            // makes the process group be the foreground
             let stdin_fd = 0;
-            unsafe { libc::ioctl(stdin_fd, libc::TIOCSCTTY, 0); }
-
-            let mut new_sigset = SigSet::empty();
-            new_sigset.add(Signal::SIGTTOU);
-            let mut old_sigset = SigSet::empty();
-            unsafe {
-                sigprocmask(SigmaskHow::SIG_BLOCK, Some(&new_sigset), Some(&mut old_sigset))
-                    .expect("[ERROR] sigprocmask block failed!");
-            }
-
-            // make this process group the foreground of the controlling terminal
             let result = unsafe { libc::tcsetpgrp(stdin_fd, getpgrp().as_raw()) };
             if result != 0 {
-                let errno = Errno::last_raw();
+                let errno = nix::errno::Errno::last_raw();
                 if errno != libc::ENOTTY {
                     eprintln!("[ERROR] tcsetpgrp failed: {}", errno);
                 }
             }
 
-            unsafe {
-                sigprocmask(SigmaskHow::SIG_SETMASK, Some(&old_sigset), None)
-                    .expect("[ERROR] sigprocmask restore failed");
-            }
-
-            // executes the shell
-            let shell = CString::new("/bin/bash").unwrap();
-            let dash_i = CString::new("-i").unwrap();
-            let args = &[shell.as_c_str(), dash_i.as_c_str()];
+            let shell = CString::new("/busybox").unwrap();
+            let arg1 = CString::new("sh").unwrap();
+            let arg2 = CString::new("-i").unwrap();
+            let args = &[shell.as_c_str(), arg1.as_c_str(), arg2.as_c_str()];
             execvp(&shell, args).expect("execvp failed");
         }
         Err(e) => {
